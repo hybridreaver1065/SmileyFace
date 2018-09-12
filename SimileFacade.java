7c0c1396e3b299930cdcf9a10dde17385aeb6f2d
hybridreaver1065 <42978468+hybridreaver1065@users.noreply.github.com>
2018-09-12 14:07:08 -0400

    Add files via upload
    This is a Smiley Face that prints in the console

---
+++ SimileFacade.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Jacob Mingis 
+ * Smiley Face :)
+ * Period Seven
+ *
+ */
+public class SimileFacade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\\\\\\\\\\");
+		System.out.println( "|*  *| ");
+		System.out.println("|  * |");
+		System.out.println("| \\_/|");
+		System.out.println("\\____/");
+	
+	
+	
+	
+	}
+
+}
